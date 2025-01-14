4583a4da3c7c9732a863146fdb943d96bb6deafb
ChristianScarabelli <scachmabol@hotmail.it>
2025-01-14 11:18:06 +0100

    ADD: global context

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { useState } from "react"
+import GlobalContext from './contexts/GlobalContext.js'
 
 // Layouts
 import BlankLayout from "./layouts/BlankLayout"
@@ -11,25 +13,31 @@ import AboutUs from './pages/AboutUs.jsx'
 import HomePage from './pages/HomePage.jsx'
 import MovieDetails from "./pages/movieDetails.jsx"
 
+
 function App() {
 
+  // stato per il loading della pagina (false di default)
+  const [isLoading, setIsLoading] = useState(false)
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/api/movies" />} />
-        <Route element={<DefaultLayout />}>
-          <Route path="/api/movies">
-            <Route index element={<HomePage />} />
-            <Route path=':id' element={<MovieDetails />} />
+    <GlobalContext.Provider value={{ isLoading, setIsLoading }} >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/api/movies" />} />
+          <Route element={<DefaultLayout />}>
+            <Route path="/api/movies">
+              <Route index element={<HomePage />} />
+              <Route path=':id' element={<MovieDetails />} />
+            </Route>
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/about" element={<AboutUs />} />
           </Route>
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/about" element={<AboutUs />} />
-        </Route>
-        <Route element={<BlankLayout />}>
-          <Route path='*' element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route element={<BlankLayout />}>
+            <Route path='*' element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GlobalContext.Provider>
   )
 }
 
